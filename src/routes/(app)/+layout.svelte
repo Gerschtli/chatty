3e64df0de8796277f40e64a158972b8a790ffaf7
Tobias Happ <tobias.happ@gmx.de>
2026-03-08 16:40:54 +0100

    rmv ws

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,28 +1,15 @@
 <script lang="ts">
-	import { wsClient } from '$lib/ws-client.svelte';
+	import { sseClient } from '$lib/sse-client.svelte';
 	import { untrack } from 'svelte';
 
 	let { children, data } = $props();
 
-	let message = $state('');
-
-	/* disable SSE for WS testing
 	// ensures that the SSE connection is closed when the user navigates away from the (app) layout (e.g. to the login page).
 	$effect(() => {
 		untrack(() => sseClient.connect(data.lastEventId));
 
 		return () => sseClient.close();
 	});
-	*/
-
-	$effect(() => {
-		untrack(() => wsClient.connect());
-
-		return () => wsClient.close();
-	});
 </script>
-
-<input bind:value={message} />
-<button onclick={() => wsClient.send(message)}>Send</button>
 
 {@render children()}
